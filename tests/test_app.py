b6d57e5d3b7ecdb7f753e719f18040299b176fac
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-13 18:53:42 +0100

    Add tests for GET/albums and GET/albums/<id>

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,57 @@
 from playwright.sync_api import Page, expect
 
-# Tests for your routes go here
-
-# === Example Code Below ===
+"""
+When I request GET/albums
+I get HTML content with the albums
+"""
+def test_get_albums(db_connection, page, test_web_address):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/albums")
+    h3_tags = page.locator("h3")
+    expect(h3_tags).to_have_text([
+        "Title: Doolittle",
+        "Title: Surfer Rosa",
+        "Title: Waterloo",
+        "Title: Super Trouper",
+        "Title: Bossanova",
+        "Title: Lover",
+        "Title: Folklore",
+        "Title: I Put a Spell on You",
+        "Title: Baltimore",
+        "Title: Here Comes the Sun",
+        "Title: Fodder on My Wings",
+        "Title: Ring Ring"
+    ])
+    paragraph_tags = page.locator("p")
+    expect(paragraph_tags).to_have_text([
+        "Released: 1989",
+        "Released: 1988",
+        "Released: 1974",
+        "Released: 1980",
+        "Released: 1990",
+        "Released: 2019",
+        "Released: 2020",
+        "Released: 1965",
+        "Released: 1978",
+        "Released: 1971",
+        "Released: 1982",
+        "Released: 1973"
+    ])
 
 """
-We can get an emoji from the /emoji page
+When I request GET /albums/<id>
+I get HTML content with the album with that id
 """
-def test_get_emoji(page, test_web_address): # Note new parameters
-    # We load a virtual browser and navigate to the /emoji page
-    page.goto(f"http://{test_web_address}/emoji")
+def test_get_album_with_id(db_connection, page, test_web_address):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/albums/1")
+    h1_tag = page.locator("h1")
+    expect(h1_tag).to_have_text(
+        "Doolittle"
+    )
+    paragraph_tag = page.locator("p")
+    expect(paragraph_tag).to_have_text(
+        "Release year: 1989\nArtist: Pixies"
+    )
 
-    # We look at the <strong> tag
-    strong_tag = page.locator("strong")
-
-    # We assert that it has the text ":)"
-    expect(strong_tag).to_have_text(":)")
-
-# === End Example Code ===
+    
