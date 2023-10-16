77c647af0eeef6f037d6830c4b0d0f74fa90d22a
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-16 13:39:34 +0100

    Improve readability of HTML content

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,18 +9,18 @@ def test_get_albums(db_connection, page, test_web_address):
     page.goto(f"http://{test_web_address}/albums")
     h3_tags = page.locator("h3")
     expect(h3_tags).to_have_text([
-        "Title: Doolittle",
-        "Title: Surfer Rosa",
-        "Title: Waterloo",
-        "Title: Super Trouper",
-        "Title: Bossanova",
-        "Title: Lover",
-        "Title: Folklore",
-        "Title: I Put a Spell on You",
-        "Title: Baltimore",
-        "Title: Here Comes the Sun",
-        "Title: Fodder on My Wings",
-        "Title: Ring Ring"
+        "Doolittle",
+        "Surfer Rosa",
+        "Waterloo",
+        "Super Trouper",
+        "Bossanova",
+        "Lover",
+        "Folklore",
+        "I Put a Spell on You",
+        "Baltimore",
+        "Here Comes the Sun",
+        "Fodder on My Wings",
+        "Ring Ring"
     ])
     paragraph_tags = page.locator("p")
     expect(paragraph_tags).to_have_text([
@@ -40,7 +40,7 @@ def test_get_albums(db_connection, page, test_web_address):
 
 """
 When I request GET /albums/<id>
-I get HTML content with the album with that id
+I get HTML content for the album with that id
 """
 def test_get_album_with_id(db_connection, page, test_web_address):
     db_connection.seed("seeds/music_library.sql")
@@ -50,8 +50,9 @@ def test_get_album_with_id(db_connection, page, test_web_address):
         "Doolittle"
     )
     paragraph_tag = page.locator("p")
-    expect(paragraph_tag).to_have_text(
-        "Release year: 1989\nArtist: Pixies"
-    )
+    expect(paragraph_tag).to_have_text([
+        "Release year: 1989",
+        "Artist: Pixies"
+    ])
 
     
