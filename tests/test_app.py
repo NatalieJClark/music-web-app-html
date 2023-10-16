755bdf4ebac097350db9bde6d8e5ae3b537ffd60
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-16 19:11:22 +0100

    Add test for link to individual album page

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,14 @@ from playwright.sync_api import Page, expect
 
 """
 When I request GET/albums
-I get HTML content with the albums
+By visiting "http://{test_web_address}/albums"
+I get HTML content with the album names
 """
 def test_get_albums(db_connection, page, test_web_address):
     db_connection.seed("seeds/music_library.sql")
     page.goto(f"http://{test_web_address}/albums")
-    h3_tags = page.locator("h3")
-    expect(h3_tags).to_have_text([
+    li_tags = page.locator("li")
+    expect(li_tags).to_have_text([
         "Doolittle",
         "Surfer Rosa",
         "Waterloo",
@@ -22,24 +23,10 @@ def test_get_albums(db_connection, page, test_web_address):
         "Fodder on My Wings",
         "Ring Ring"
     ])
-    paragraph_tags = page.locator("p")
-    expect(paragraph_tags).to_have_text([
-        "Released: 1989",
-        "Released: 1988",
-        "Released: 1974",
-        "Released: 1980",
-        "Released: 1990",
-        "Released: 2019",
-        "Released: 2020",
-        "Released: 1965",
-        "Released: 1978",
-        "Released: 1971",
-        "Released: 1982",
-        "Released: 1973"
-    ])
 
 """
 When I request GET /albums/<id>
+By visiting "http://{test_web_address}/albums/1"
 I get HTML content for the album with that id
 """
 def test_get_album_with_id(db_connection, page, test_web_address):
@@ -49,10 +36,47 @@ def test_get_album_with_id(db_connection, page, test_web_address):
     expect(h1_tag).to_have_text(
         "Doolittle"
     )
-    paragraph_tag = page.locator("p")
-    expect(paragraph_tag).to_have_text([
-        "Release year: 1989",
+    release_year_tag = page.locator(".t-release-year")
+    expect(release_year_tag).to_have_text(
+        "Release year: 1989"
+    )
+    artist_tag = page.locator(".t-artist")
+    expect(artist_tag).to_have_text(
         "Artist: Pixies"
-    ])
+    )
 
-    
+"""
+When we click on the link "Surfer Rosa" on the /albums page
+We get a page with the id of Surfer Rosa that shows information about that album
+"""
+def test_visit_album_shows_page(db_connection, page, test_web_address):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/albums")
+    page.click("text='Surfer Rosa'")
+
+    h1_tag = page.locator("h1")
+    expect(h1_tag).to_have_text("Surfer Rosa")
+
+    release_year_tag = page.locator(".t-release-year")
+    expect(release_year_tag).to_have_text(
+        "Release year: 1988"
+    )
+
+    artist_tag = page.locator(".t-artist")
+    expect(artist_tag).to_have_text(
+        "Artist: Pixies"
+    )
+
+"""
+When we visit "http://{test_web_address}/albums/1"
+And click 'Go back to album list'
+We go back to "http://{test_web_address}/albums"
+"""
+def test_visit_album_show_page_and_go_back(db_connection, page, test_web_address):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/albums")
+    page.click("text='Surfer Rosa'")
+    page.click("text='Go back to album list'")
+    h1_tag = page.locator("h1")
+    expect(h1_tag).to_have_text("Albums")
+
